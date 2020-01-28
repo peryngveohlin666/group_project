8a01c502337628a97a94301a5089495dd549c6b1
peryngveohlin666 <46087170+peryngveohlin666@users.noreply.github.com>
2020-01-28 08:01:28 +0000

    fixed the css and html locations, made them work with django and put the in the right places accordingly (base.html, extends etc.), started setting up the django-guardian library for authentication and grouping. Also created a models.py for people to create their database objects in.

--- ats/ats/settings.py
+++ ats/ats/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'guardian',
 ]
 
 MIDDLEWARE = [
@@ -101,6 +102,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend', # default
+    'guardian.backends.ObjectPermissionBackend',
+)
 
 
 # Internationalization
