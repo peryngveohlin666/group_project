e8cae757bd8b8883ca58ee014555b21865633c52
peryngveohlin666 <46087170+peryngveohlin666@users.noreply.github.com>
2020-02-13 12:09:11 +0000

    we can assign the blanks

--- ats/blank_system/views.py
+++ ats/blank_system/views.py
@@ -3,10 +3,9 @@ from django.contrib import auth
 from django.http import HttpResponse
 from django.template import RequestContext
 import ats.urls
-from blank_system.forms import blank_form
+from blank_system.forms import blank_form, assign_blank_form
 from blank_system.models import blank
 # Create your views here.
-
 
 
 def create_blanks(request):
@@ -24,7 +23,8 @@ def create_blanks(request):
             return render(request, "create_blanks.html")
     else:
         form = blank_form
-        return render(request, "create_blanks.html", {'form':form})
+        return render(request, "create_blanks.html", {'form': form})
+
 
 def blanks(request):
     blanks = blank.objects.all()
@@ -33,3 +33,23 @@ def blanks(request):
     }
     return render(request, 'blanks.html', context)
 
+
+def assign_blanks(request):
+    if request.method == 'POST':
+        form = assign_blank_form(data=request.POST)
+        from_value = request.POST.get("from_value", "")
+        to_value = request.POST.get("to_value", "")
+        blanks = blank.objects.all()
+        if form.is_valid():
+            for b in blanks:
+                if b.number >= int(from_value) and b.number <= int(to_value):
+                    print(form.instance.advisor)
+                    b.advisor = form.instance.advisor
+                    b.save()
+                if b.number == int(to_value):
+                    break
+                print(b.number)
+        return render(request, 'assign_blanks.html')
+    else:
+        form = assign_blank_form()
+        return render(request, 'assign_blanks.html', {'form':form})
